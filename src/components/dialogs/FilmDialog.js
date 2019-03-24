c7fecc730ade1c299c8fb40c08e9bf2b3660f0d7
Sergey <relzet69@gmail.com>
2019-03-25 01:11:37 +0200

    add final form

--- src/components/dialogs/FilmDialog.js
+++ src/components/dialogs/FilmDialog.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,74 +7,60 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import {connect} from "react-redux";
 import {resetSelectedFilm, saveFilm} from "../../actions";
 import PropTypes from "prop-types";
+import {Field, Form} from "react-final-form";
+import {TextField} from 'final-form-material-ui';
 
 class FilmDialog extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      film: this.props.initialFilm
-    }
-  }
-
-  resetState = () => {
-    this.setState({film: null})
-  }
-
   handleClose = () => {
     this.props.resetSelectedFilm()
-    this.resetState()
   };
-
-  handleSave = () => {
-    this.props.saveFilm(this.state.film)
-    this.resetState()
-  };
-
-  handleChange = name => event => this.setState({
-    film: {
-      ...this.state.film,
-      [name]: event.target.value
-    }
-  })
 
   render() {
-    const {film} = this.state;
+    const {initialFilm} = this.props
     return (
       <div>
         <Dialog
-          open={!!film}
+          open={!!initialFilm}
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Edit film</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="Title"
-              label="Title"
-              value={film.Title}
-              onChange={this.handleChange('Title')}
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="Year"
-              label="Year"
-              value={film.Year}
-              onChange={this.handleChange('Year')}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleSave} color="primary">
-              Save
-            </Button>
-          </DialogActions>
+          <Form
+            onSubmit={this.props.saveFilm}
+            initialValues={{...initialFilm}}
+            render={({handleSubmit}) => (
+              <Fragment>
+                <form onSubmit={handleSubmit} noValidate>
+                  <DialogTitle id="form-dialog-title">Edit film</DialogTitle>
+                  <DialogContent>
+                    <Field
+                      autoFocus
+                      fullWidth
+                      name="Title"
+                      component={TextField}
+                      type="text"
+                      label="Title"
+                    />
+                    <Field
+                      fullWidth
+                      name="Year"
+                      component={TextField}
+                      type="number"
+                      label="Year"
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                      Cancel
+                    </Button>
+                    <Button type="submit" color="primary">
+                      Save
+                    </Button>
+                  </DialogActions>
+                </form>
+              </Fragment>
+            )}
+          />
         </Dialog>
       </div>
     );
